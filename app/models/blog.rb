8de2f721da711d3c50614b033cb4d33ff97978d7
tomoaki1169 <0111tomoaki@gmail.com>
2020-09-11 10:17:29 +0900

    haml変更

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,5 @@ class Blog < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :start_time, presence: true
+  belongs_to :blog
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'blogs#index'
   resources :blogs
+  resources :users,only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
